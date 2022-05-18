e093ae06858f5d141ba4672f85a42c9190238e19
Pedro <contato.pedromoc@gmail.com>
2022-05-18 13:20:40 -0300

    primeira atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { pipe, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-http';
+  readonly uri: string;
+
+  constructor(private http: HttpClient) {
+    this.uri = 'http://localhost:3000/comments';
+  }
+
+  public getMethod() {
+    this.http.get(this.uri).subscribe(res => console.log(res), error => console.log('erro ao buscar usuarios'));
+  }
+
+  public postMethod() {
+    let body = 'Novo comentario aqui';
+
+    this.http.post(this.uri, { id: null, body: body }).subscribe({
+      next: res => console.log(res),
+      error: err => console.log(err)
+    });
+  }
 }
